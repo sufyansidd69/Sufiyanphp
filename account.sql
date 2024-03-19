21bd63a91fe1afba061079d51c69d649930e7f26
Sufyan Siddiqui <siddsufyan3@gmail.com>
2024-03-20 01:05:17 +0500

    delect

--- account.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `account` (`id`, `fName`, `email`, `CS`, `OOP`, `DSA`, `CP`, `Total`, `per`, `Grade`) VALUES (19, 'sufiyan', 'deathpool638@gmail.com', '57', '48', '100', '100', '305', '76.25', 'B');
-INSERT INTO `account` (`id`, `fName`, `email`, `CS`, `OOP`, `DSA`, `CP`, `Total`, `per`, `Grade`) VALUES (20, 'Altaf Bhai', 'MqmP@gmail.com', '100', '100', '100', '100', '400', '100', 'A+');
-INSERT INTO `account` (`id`, `fName`, `email`, `CS`, `OOP`, `DSA`, `CP`, `Total`, `per`, `Grade`) VALUES (21, 'Bilal Imran', 'Bilal2@gmail.com', '82', '65', '67', '100', '314', '78.5', 'B');
-INSERT INTO `account` (`id`, `fName`, `email`, `CS`, `OOP`, `DSA`, `CP`, `Total`, `per`, `Grade`) VALUES (22, 'sufiyan', 'deathpool638@gmail.com', '57', '48', '90', '100', '295', '73.75', 'B');

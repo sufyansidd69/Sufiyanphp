45950c36c1658c7f663627376411daa06a7db0cc
Sufyan Siddiqui <135153054+sufyansidd69@users.noreply.github.com>
2024-03-20 00:39:25 +0500

    Create Data Base Connection IN PHP

---
+++ dbc.php
@@ -0,0 +1,13 @@
+<?php 
+define("DB_HOST",'localhost');
+define("DB_USER",'root');
+define("DB_PASSWORD",'');
+define("DB_NAME",'Sufiyanphp');
+
+$conn=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if(!$conn){
+    echo "Unable to connect". mysqli_error();
+    die();
+}
+
+?>
